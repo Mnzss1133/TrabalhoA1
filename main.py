d2806967a8131a2dc8d96a9aea2d7cfcac4933f4
Joao Pedro Passos Ribeiro de Menezes <86020181+Mnzss1133@users.noreply.github.com>
2022-10-08 23:01:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -219,36 +219,11 @@ def musicas_menos_tocada_da_história():
 
 
 
-#Grupo 2
-#Usando o site ouvirmusica.com.br para conseguir pegas as letras das músicas
-url2='https://www.ouvirmusica.com.br/justin-bieber/'
-ouvirmusica = requests.get(url2)
-conteudo = ouvirmusica.content
-site2= BeautifulSoup(conteudo, 'html.parser')
-
-#Dentro do site das letras o comando entrará em cada uma das músicas e obterá a sua letra e colocará em uma lista
-site2_musicas = site2.find('ol', attrs={'class':'list'})
-
-lista_letras = []
-
-for links in site2_musicas.findAll('a'):
-    link_musica = 'https://www.ouvirmusica.com.br' + links['href']
-
-
-    request_ouvirmusica = requests.get(link_musica)
-    conteudo_letra = request_ouvirmusica.content
-    site_letra = BeautifulSoup(conteudo_letra, 'html.parser')
-
-    letras_jb = site_letra.find('div', attrs={'class':'cnt'})
-    lista_letras.append(letras_jb.get_text())
-#print(lista_letras) 
-
-
 # Grupo 3 
 def media():
     DF_principal_media = DF_principal['Duração'].mean()
     return f'f A media da duracao de todas as músicas é de {DF_principal_media}'
 print(media)
 
-#Grupo_1_Parte_3
+
 
